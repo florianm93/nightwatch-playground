48723f7f9c0329af5ef54cbea5202669cc935a83
Florian Macherey <Florian.Macherey@vzbv.de>
2018-01-30 15:56:11 +0100

    tests: fixed testcase

--- tests/test.js
+++ tests/test.js
@@ -1,10 +1,13 @@
 module.exports = {
   'Demo test with 2 inputs change a div' : function (browser) {
     browser
-      .url('http://127.0.0.1:8080/src')
+      .url('http://127.0.0.1:8080/src/index.html')
       .waitForElementVisible('body', 1000)
+      .assert.visible('#somediv')
       .assert.containsText('#somediv', 'Welcome')
+      .clearValue('#tb1')
       .setValue('#tb1', 'Foo')
+      .clearValue('#tb2')
       .setValue('#tb2', 'Bar')
       .click('#btn1')
       .pause(1000)
